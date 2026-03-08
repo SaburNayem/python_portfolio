137e3523e7f2d244c9e00b7d193b86f6968d7f9e
nayemsabur <mdsabur9991@gmail.com>
2026-03-08 17:31:08 +0600

    Build Django 3D lamp portfolio with animated interactive scene

---
+++ portfolio/urls.py
@@ -0,0 +1,9 @@
+﻿from django.urls import path
+
+from .views import api_portfolio, contact_submit, home
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("contact/", contact_submit, name="contact_submit"),
+    path("api/portfolio/", api_portfolio, name="api_portfolio"),
+]
